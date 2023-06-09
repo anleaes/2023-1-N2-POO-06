5087812a9d3d50639e62e3e4ef488c9569a4addd
Gabriel Molon <gabrielmolon@hotmail.com>
2023-06-08 22:01:22 -0300

    Criação dos atributos no models e serializer e url

--- apps/farmacia/models.py
+++ apps/farmacia/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Farmacia(models.Model):
+    estoque = models.CharField('Estoque', max_length=50)
+    medicamento = models.CharField('Medicamento', max_length=50)
+    lotes = models.CharField('Lotes', max_length=50)
+
+    class Meta:
+        verbose_name = 'Farmácia'
+        verbose_name_plural = 'Farmácias'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.medicamento
