5fa1d73f489c226b4d8e1704f6fe13c510e4f8c3
sennafilipe42 <filipesennati@gmail.com>
2023-06-09 20:38:41 -0300

    ajuste model sistema

--- apps/sistema/models.py
+++ apps/sistema/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from paciente.models import Paciente
+from medico.models import Medico
+from prontuario.models import Prontuario
 
 class Sistema(models.Model):
-    paciente = models.CharField(max_length=100)
-    medico = models.CharField(max_length=100)
-    prontuario = models.TextField()
+    paciente = models.ForeignKey(Paciente, verbose_name='Paciente', on_delete=models.CASCADE)
+    medico = models.ForeignKey(Medico, verbose_name='Medico', on_delete=models.CASCADE)
+    prontuario = models.ForeignKey(Prontuario, verbose_name='Prontuario', on_delete=models.CASCADE)
  
     class Meta:
         verbose_name = 'Sistema'
         verbose_name_plural = 'Sistemas'
         ordering = ['id']
 
-    def __str__(self):
-        return f"{self.paciente},{self.medico},{self.prontuario}"
+    def str(self):
+        return f"Paciente: {self.paciente.name}, Médico: {self.medico.nome}, Prontuário: {self.prontuario}"
 
