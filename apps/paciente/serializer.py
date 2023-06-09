2eb4a945baf581742b2ca8677a26c045ce965a46
joão <jv.davila@outlook.com>
2023-06-08 22:04:03 -0300

    serializer fix

--- apps/paciente/serializer.py
+++ apps/paciente/serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields = ('name', 'cpf', 'data_nascimento_', 'endereco', 'historico', 'sexo')
+        fields = ['name', 'cpf', 'data_nascimento_', 'endereco', 'historico', 'sexo']
