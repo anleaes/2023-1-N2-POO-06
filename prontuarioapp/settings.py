48c9abbd28f4be1f348679c3fd9ed3341dd545e8
joão <jv.davila@outlook.com>
2023-06-08 23:43:51 -0300

    corrigindo de novo

--- prontuarioapp/settings.py
+++ prontuarioapp/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'sistema.apps.SistemaConfig',
     'paciente.apps.PacienteConfig',
-    'medico.apps.SistemaConfig',
+    'medico.apps.MedicoConfig',
 ]
 
 MIDDLEWARE = [
