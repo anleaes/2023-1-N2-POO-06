e26cc17357681b94beae884689ca809bde64394b
sennafilipe42 <filipesennati@gmail.com>
2023-06-08 21:39:12 -0300

    Ajuste nos arquivos gerais

--- apps/sistema/models.py
+++ apps/sistema/models.py
@@ -5,6 +5,11 @@ class Sistema(models.Model):
     paciente = models.CharField(max_length=100)
     medico = models.CharField(max_length=100)
     prontuario = models.TextField()
+    
+    class Meta:
+        verbose_name = 'Sistema'
+        verbose_name_plural = 'Sistemas'
+        ordering = ['id']
 
     def __str__(self):
         return f"{self.paciente},{self.medico},{self.prontuario}"
