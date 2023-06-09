ff394d569386439f60882ccab14ee9cbd85494ed
joão <jv.davila@outlook.com>
2023-06-08 23:41:32 -0300

    serializer - classe medico

--- apps/medico/views.py
+++ apps/medico/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Medico
+from rest_framework import viewsets
+from .serializer import Medico
 
 # Create your views here.
+
+class MedicoViewSet(viewsets.ModelViewSet):
+    queryset = Medicot.objects.all()
+    serializer_class = MedicoSerializer
