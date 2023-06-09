b1752fa865ed301b021aa28db8a298e2ded8e98d
joão <jv.davila@outlook.com>
2023-06-09 20:42:43 -0300

    test return to string

--- apps/medico/models.py
+++ apps/medico/models.py
@@ -3,10 +3,12 @@ from paciente.models import Paciente
 
 # Create your models here.
 
+
 class Medico(models.Model):
     nome = models.CharField('Nome', max_length=50)
     especialidade = models.CharField('Especialidade', max_length=50)
-    paciente = models.ForeignKey(Paciente, verbose_name='Paciente', on_delete=models.CASCADE)
+    paciente = models.ForeignKey(
+        Paciente, verbose_name='Paciente', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Medico'
@@ -14,5 +16,4 @@ class Medico(models.Model):
         ordering = ['id']
 
     def __str__(self):
-        return self.nome
-
+        return self
