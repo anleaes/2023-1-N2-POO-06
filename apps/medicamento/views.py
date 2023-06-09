7daf737f0e3be17858551c0a5d5bfb2d4e88aa80
CaetanoMS <camaso19hz@gmail.com>
2023-06-09 18:59:49 -0300

    ConfigMedicamento

--- apps/medicamento/views.py
+++ apps/medicamento/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Medicamento
+from rest_framework import viewsets
+from .serializer import MedicamentoSerializer
 
 # Create your views here.
+
+class MedicamentoViewSet(viewsets.ModelViewSet):
+    queryset = Medicamento.objects.all()
+    serializer_class = MedicamentoSerializer
