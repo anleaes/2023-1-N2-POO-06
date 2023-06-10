71cae91e2be1cb37ec51a442b0dd590a4478abcf
Gabriel Molon <gabrielmolon@hotmail.com>
2023-06-10 15:03:05 -0300

    Ajustes para acesso web

--- apps/farmacia/urls.py
+++ apps/farmacia/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'farmacia'
 
 router = routers.DefaultRouter()
-router.register('farmacia', views.MedicamentoViewSet, basename='farmacia')
+router.register('farmacia', views.FarmaciaViewSet, basename='farmacia')
 
 urlpatterns = [
     path('', include(router.urls) )
