18ad863f057a81f14d51c1e332233ad3c01597bd
Gabriel Molon <gabrielmolon@hotmail.com>
2023-06-10 22:06:46 -0300

    Configuração Classe Models

--- apps/prescricao/models.py
+++ apps/prescricao/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from medicamento.models import Medicamento
 
 # Create your models here.
+
+class Prescricao(models.Model):
+    dosagem = models.CharField('Dosagem', max_length=50)
+    medicamento = models.ForeignKey('Medicamento', on_delete=models.CASCADE)
+    frequencia = models.CharField('Frequência', max_length=50)
+    tratamento = models.CharField('Tratamento', max_length=100)
+
+    class Meta:
+        verbose_name = 'Prescrição'
+        verbose_name_plural = 'Prescrições'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'Prescrição {self.id}'
+
