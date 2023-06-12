6a283f90e40dcf8d1bfc3132921703d970acae67
CaetanoMS <camaso19hz@gmail.com>
2023-06-12 18:26:51 -0300

    Alterações na configuração do prontuario

--- apps/prontuario/models.py
+++ apps/prontuario/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from paciente.models import Paciente
-# from prescricao.models import Prescricao
+from prescricao.models import Prescricao
 
 class Prontuario(models.Model):
     paciente = models.ForeignKey(Paciente, verbose_name='Paciente', on_delete=models.CASCADE)
     data_criacao = models.DateField('Data de Criação')
-    sintomas = models.CharField('Frequência', max_length=100)
-    diagnostico = models.CharField('Tratamento', max_length=100)
-    # prescicao = models.ForeignKey(Prescricao, verbose_name='Prescricao', on_delete=models.CASCADE)
+    sintomas = models.CharField('Sintomas', max_length=100)
+    diagnostico = models.CharField('Diagnóstico', max_length=100)
+    prescicao = models.ForeignKey(Prescricao, verbose_name='Prescricao', on_delete=models.CASCADE)
 
     
     class Meta:
